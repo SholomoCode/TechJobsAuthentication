b076e0dc96199c0df002c99b8f14c299128e1143
SholomoCode <worksolom5@gmail.com>
2023-04-18 10:38:48 -0500

    add identity

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using TechJobsAuthentication.Data;
+using Microsoft.AspNetCore.Identity;
+//using TechJobsAuthentication.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+/*var connectionString = builder.Configuration.GetConnectionString("JobDbContextConnection");builder.Services.AddDbContext<JobDbContext>(options =>
+    options.UseSqlServer(connectionString));builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<JobDbContext>();
+// Add services to the container.*/
 builder.Services.AddControllersWithViews();
 
 //--- MySql connection
@@ -13,11 +17,27 @@ builder.Services.AddControllersWithViews();
 // working with the .NET 6 (specifically the lack of a Startup.cs)
 //https://learn.microsoft.com/en-us/aspnet/core/migration/50-to-60-samples?view=aspnetcore-6.0#add-configuration-providers
 
-var connectionString = "server=localhost;user=techjobs_auth;password=ILoveTechJobs;database=techjobs_auth";
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+var connectionString = "server=localhost;user=techjobs_auth;password=techjobs_auth;database=techjobs_auth";
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
 
 builder.Services.AddDbContext<JobDbContext>(dbContextOptions => dbContextOptions.UseMySql(connectionString, serverVersion));
 //--- end of connection syntax
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDefaultIdentity<IdentityUser>
+(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<JobDbContext>();
+
+
+
 
 
 var app = builder.Build();
@@ -34,8 +54,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization()
+app.MapRazorPages();
+app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
